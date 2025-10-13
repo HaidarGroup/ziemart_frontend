1393db6368f48639d1dd6c9d4946fa3f44469079
mdhrarrf <haidarmeeteng@gmail.com>
2025-10-13 14:35:06 +0700

    nambahin homepage, tapi belum rapih

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:ziemart_frontend/view/pages/homepage.dart';
-import 'package:ziemart_frontend/view/pages/registerPage.dart';
-import 'package:ziemart_frontend/view/pages/splashPage.dart';
-import 'package:ziemart_frontend/view/pages/loginPage.dart';
+  import 'package:flutter/material.dart';
+  import 'package:get/get.dart';
+  import 'package:ziemart_frontend/view/pages/homePage.dart';
+  import 'package:ziemart_frontend/view/pages/registerPage.dart';
+  import 'package:ziemart_frontend/view/pages/splashPage.dart';
+  import 'package:ziemart_frontend/view/pages/loginPage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Splash Example',
-      initialRoute: '/splash',
-      routes: {
-        '/splash': (context) => const SplashPage(),
-        '/registerPage': (context) => const RegisterPage(),
-        '/loginPage': (context) => const LoginPage(),
-        '/homePage': (context) => const HomePage(),
-      },
-    );
+  void main() {
+    runApp(const MyApp());
   }
-}
+
+  class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Splash Example',
+        initialRoute: '/splash',
+        routes: {
+          '/splash': (context) => const SplashPage(),
+          '/registerPage': (context) => const RegisterPage(),
+          '/loginPage': (context) => const LoginPage(),
+          '/homePage': (context) => HomePage(),
+        },
+      );
+    }
+  }
